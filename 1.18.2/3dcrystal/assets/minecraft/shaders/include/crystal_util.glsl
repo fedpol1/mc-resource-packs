1f97b39f5d1c1f7296e99c07a4ae1f24715a8d5b
fedpol1 <fedpolone@gmail.com>
2022-03-01 13:25:46 -0500

    Remove unused parallel function from 3dcrystal

--- 1.18.2/3dcrystal/assets/minecraft/shaders/include/crystal_util.glsl
+++ 1.18.2/3dcrystal/assets/minecraft/shaders/include/crystal_util.glsl
@@ -4,10 +4,6 @@
 #define LIGHT0_DIRECTION vec3(0.2, 1.0, -0.7) // Default light 0 direction everywhere except in inventory
 #define LIGHT1_DIRECTION vec3(-0.2, 1.0, 0.7) // Default light 1 direction everywhere except in nether and inventory
 
-bool parallel(vec3 v1, vec3 v2) {
-    return v1 == -v2 || v1 == v2;
-}
-
 vec3 get_offset(vec4 color) {
 	float a = floor(color.b * 7.95);
 	return (vec3(mod(a, 2.0), float(a - 4.0 * float(a >= 4.0) >= 2.0), float(a >= 4.0)) - 0.5) * 2.0;
